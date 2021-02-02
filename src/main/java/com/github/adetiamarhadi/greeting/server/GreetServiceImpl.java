565ac13a0e6beed0b7f1624567d5303797730ff0
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-02-02 11:23:07 +0700

    unary api server implementation

---
+++ src/main/java/com/github/adetiamarhadi/greeting/server/GreetServiceImpl.java
@@ -0,0 +1,27 @@
+package com.github.adetiamarhadi.greeting.server;
+
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
+import io.grpc.stub.StreamObserver;
+
+public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
+
+    @Override
+    public void greet(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
+
+        Greeting greeting = request.getGreeting();
+
+        String firstName = greeting.getFirstName();
+
+        String result = "Hello " + firstName;
+
+        GreetResponse response = GreetResponse.newBuilder()
+                .setResult(result)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
