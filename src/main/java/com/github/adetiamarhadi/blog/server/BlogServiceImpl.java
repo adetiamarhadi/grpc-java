91b9bd5662d2458058be9bb8789647a26de7d6d8
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-02-08 13:30:46 +0700

    create blog server

--- src/main/java/com/github/adetiamarhadi/blog/server/BlogServiceImpl.java
+++ src/main/java/com/github/adetiamarhadi/blog/server/BlogServiceImpl.java
@@ -1,6 +1,45 @@
 package com.github.adetiamarhadi.blog.server;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.proto.blog.Blog;
 import com.proto.blog.BlogServiceGrpc;
+import com.proto.blog.CreateBlogRequest;
+import com.proto.blog.CreateBlogResponse;
+import io.grpc.stub.StreamObserver;
+import org.bson.Document;
 
 public class BlogServiceImpl extends BlogServiceGrpc.BlogServiceImplBase {
+
+    private MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+    private MongoDatabase mongoDatabase = this.mongoClient.getDatabase("mydb");
+    private MongoCollection<Document> mongoCollection = this.mongoDatabase.getCollection("blog");
+
+    @Override
+    public void createBlog(CreateBlogRequest request, StreamObserver<CreateBlogResponse> responseObserver) {
+
+        Blog blog = request.getBlog();
+
+        Document document = new Document("author_id", blog.getAuthorId())
+                .append("title", blog.getTitle())
+                .append("content", blog.getContent());
+
+        this.mongoCollection.insertOne(document);
+
+        String id = document.getObjectId("_id").toString();
+
+        CreateBlogResponse blogResponse = CreateBlogResponse.newBuilder()
+                .setBlog(Blog.newBuilder()
+                        .setAuthorId(blog.getAuthorId())
+                        .setContent(blog.getContent())
+                        .setTitle(blog.getTitle())
+                        .setId(blog.getId())
+                        .build())
+                .build();
+
+        responseObserver.onNext(blogResponse);
+        responseObserver.onCompleted();
+    }
 }
